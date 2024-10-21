cae1e436463f380913bc3e6ef30021bc4bac4cab
ap45 <arpit.parekh0104@gmail.com>
2024-10-21 12:40:14 -0400

    updated urls.py

--- backend/urls.py
+++ backend/urls.py
@@ -22,12 +22,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 
-
-
-# urlpatterns = [
-#     path('admin/', admin.site.urls),
-# ]
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('student_course.urls')),
